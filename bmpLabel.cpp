5b67de9a4a1ecd32307828f3adf220d1ccd4030b
Jim Lee <gitHub@leftcoast.biz>
2021-11-01 22:32:09 -0700

    rewrote bmpLabel to use offscreen. Worked out well. was a pain to figure out.

--- bmpLabel.cpp
+++ bmpLabel.cpp
@@ -1,64 +1,66 @@
 
-#include "bmpLabel.h"
+#include <bmpLabel.h>
+#include <offscreen.h>
 
-bmpLabel::bmpLabel(int inX,int inY,int width,int height,char* inText,bmpPipe* inBackdrop) 
-  : label(inX,inY,width,height,inText) { backdrop = inBackdrop; fastDraw = true; }
-
-
-bmpLabel::~bmpLabel(void) {  }
-
+bmpLabel::bmpLabel(int inX,int inY,int width,int height,char* inText,char* bmpPath) 
+  : label(inX,inY,width,height,inText) {
   
-void bmpLabel::setFastDraw(bool fast) { fastDraw = fast; }
+  backdrop = new bmpObj(inX,inY,width,height,bmpPath);	// Create a new bmpObj for our backdrop.
+  setColors(&textColor);											// Cheaty trick to set transparent drawing.
+}
+
+
+bmpLabel::~bmpLabel(void) { if (backdrop) delete(backdrop); }
 
 
 // We go through all this nonsense so we can redraw ourselves on every line.
 // This actually hides the major flicker issue we would have otherwise.
-void bmpLabel::smoothDraw(void) {
+// 
+
+bool bmpLabel::offScreenDraw(void) {
   
-  File	bmpFile;
-  int 	trace;
-  int 	endY;
-  int 	srcY;
-  rect 	temp;
-  
-  if (backdrop) {
-  	bmpFile = backdrop->getFile();
-  	if (bmpFile) {
-  		temp = (rect) *this;											// Get our local rectangle.
-  		temp.x = screen->gX(temp.x);								// Convert to global.
-			temp.y = screen->gY(temp.y);
-    	backdrop->setSourceRect(&temp);							// Reading background is global.                                                                                     
-  		endY = y+height;
-  		srcY = temp.y;													// Anything for background global.
-  		for (trace=y; trace<endY;trace++) {						// Drawing y, local.
-    		bmpFile.seek(backdrop->filePtr(temp.x,srcY++));	// These have to be forced global.
-    		backdrop->drawLine(bmpFile,x,trace);				// Drawing is all local.
-  			label::drawSelf();										// Local.
-  			//screen->drawRect(x,y,width,height,&red);
-  		} 
-  		bmpFile.close();
-  	}
-  }      
+	rect			temp;
+	offscreen	vPort;
+	bitmap		ourBMap;
+	int			orgX;
+	int			orgY;
+	
+	if (backdrop) {
+		if (ourBMap.setSize(width,height)) {				// If we can allocate the RAM for the bitmap..
+			temp = (rect) *this;									// Get our local rectangle.
+			temp.x = screen->gX(temp.x);						// Convert to globals.
+			temp.y = screen->gY(temp.y);						//
+			backdrop->setLocation(x,y);						// Just in case we moved..
+			backdrop->setSourceOffset(temp.x,temp.y);		// Point the bitmap to the bits in the file.
+			vPort.beginDraw(&ourBMap,temp.x,temp.y);		// Start up offscreen drawing.
+			backdrop->draw();										// Draw the background to it..
+			label::drawSelf();									// Get the text in there.
+			vPort.endDraw();										// Restore normal drawing.
+			screen->blit(x,y,&ourBMap);						// Blast the bitmap out to the screen.
+			return true;
+		}
+	}
+	return false;
 }
 
 
-void bmpLabel::fasterDraw(void) {
+void bmpLabel::ezDraw(void) {
 	
 	rect 	temp;
 			
-	temp = (rect) *this;					// Get our local rectangle.
-	temp.x = screen->gX(temp.x);		// Convert to global.
+	temp = (rect) *this;									// Get our local rectangle.
+	temp.x = screen->gX(temp.x);						// Convert to global.
 	temp.y = screen->gY(temp.y);
-	backdrop->setSourceRect(&temp);
-	backdrop->drawImage(x,y);			// Draw in local.
-	label::drawSelf();
+	backdrop->setLocation(x,y);						// Just in case we moved..
+	backdrop->setSourceOffset(temp.x,temp.y);		// Point the bitmap to the bits in the file.
+	backdrop->draw();										// Draw in local.
+	label::drawSelf();									// Blast it out to the screen.
 }
 
 
 void bmpLabel::drawSelf(void){
 
-	if (fastDraw) 
-		fasterDraw();
-	else
-		smoothDraw();
+	if (!offScreenDraw()) {
+		ezDraw();
+	}
 }
--- bmpLabel.h
+++ bmpLabel.h
@@ -1,8 +1,8 @@
 #ifndef bmpLabel_h
 #define bmpLabel_h
 
-#include <bmpPipe.h>
 #include <label.h>
+#include <bmpObj.h>
 
 // This was written for the old vacuum gauge. It had a graphic "skin" and
 // The text was colored to look like it was glowing under the glass. This
@@ -10,20 +10,22 @@
 // would just destroy the effect.
 //
 // Oddly, it worked.
+//
+// Oct/2021 : bmpPipe is gone, offscreen drawing is here. The guts are all different now.
 
 class bmpLabel : public label {
 
 	public:
-  				bmpLabel(int inX,int inY,int width,int height,char* inText,bmpPipe* inBackdrop);
+  				bmpLabel(int inX,int inY,int width,int height,char* inText,char* bmpPath);
 	virtual	~bmpLabel(void);
-  
-  				void		setFastDraw(bool fast);
-  				void		smoothDraw(void);
-  				void		fasterDraw(void);
+	
+	protected:
+	
 	virtual	void		drawSelf(void);
+	 			bool		offScreenDraw(void);
+  				void		ezDraw(void);
 
-				bmpPipe*	backdrop;
-				bool		fastDraw;
+				bmpObj*	backdrop;
 };
 
 #endif
